4927c3ad0102994b7e3c10da59cbaae33cf7a4a4
Ryan Adolf <ryanadolf123@gmail.com>
2019-11-23 20:50:39 -0800

    Use atlas database

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/celo', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/celo', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://celo:celoproject@blockchainatberkeley-tosn8.azure.mongodb.net/celo?retryWrites=true&w=majority', { useNewUrlParser: true })
 
 module.exports = mongoose;
